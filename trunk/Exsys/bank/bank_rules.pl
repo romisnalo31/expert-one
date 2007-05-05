bdfd62c744dce30762d71bcac4c0bc3beb909851
astudnev <astudnev@b392081c-fb25-0410-8b55-f1b6921ca190>
2007-05-05 20:06:10 +0000

    Initial import.

---
+++ trunk/Exsys/bank/bank_rules.pl
@@ -0,0 +1,13 @@
+
+:- include('bank/bank_product.pl').
+:- include('bank/bank_product2.pl').
+
+serves(absolut, moscow).
+serves(absolut, peter).
+
+require_document(absolut,ndfl2).
+require_document(absolut,trudovaja).
+
+ok_age(absolut,Age) :- 
+    !,
+ 	between(25,47,Age).
---
+++ trunk/Exsys/main.pl
@@ -0,0 +1,16 @@
+% main module for expert system
+:- include('goal.pl').
+:- include('bank/bank_rules.pl').
+:- include('all/all_rules.pl').
+:- include('questions.pl').
+
+:- dynamic
+        known/3.
+
+main :-
+    findall(Product, credit(aleksey, Product), Result),
+    write('Credit products are:'), write(Result), nl.
+    
+main :-
+    write('No answer found'), nl.
+	
